fc9edbd351753570de7ac733f9b82534f443cc38
Mitrofanov <ounceemitr@gmail.com>
2022-12-06 22:20:08 +0400

    123

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("<h4>Hello</h4>")
+    return render(request, 'main/index.html')
